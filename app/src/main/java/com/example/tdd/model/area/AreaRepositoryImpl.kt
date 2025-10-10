c5392d91f9018d1aac5b53ea0621f2810f5db256
choimingyu <alsrb1315093@gmail.com>
2025-10-10 16:08:27 +0900

    base 브랜치 수정

--- app/src/main/java/com/example/tdd/model/area/AreaRepositoryImpl.kt
+++ app/src/main/java/com/example/tdd/model/area/AreaRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.example.tdd.model.area
 
+import com.example.tdd.model.common.ErrorHandler
+
 class AreaRepositoryImpl(
     private val api: AreaApiService,
-    private val mapper: AreaMapper
+    private val mapper: AreaMapper,
+    private val errorHandler: ErrorHandler
 ): AreaRepository {
     override suspend fun getAreaCode(os: String, app: String, serviceKey: String): List<Area> = emptyList()
 }
--- app/src/main/java/com/example/tdd/viewmodel/area/AreaViewModel.kt
+++ app/src/main/java/com/example/tdd/viewmodel/area/AreaViewModel.kt
@@ -4,12 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tdd.model.area.Area
 import com.example.tdd.model.area.AreaRepository
+import com.example.tdd.model.common.ErrorHandler
 import com.example.tdd.ui.common.ResourceState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class AreaViewModel(private val repo: AreaRepository): ViewModel() {
+class AreaViewModel(
+    private val repo: AreaRepository,
+    private val errorHandler: ErrorHandler
+): ViewModel() {
 
     private var _state = MutableStateFlow<ResourceState<List<Area>>>(ResourceState.Loading)
     val state: StateFlow<ResourceState<List<Area>>> = _state
